a2434f35c471a59299cf8d7f969231d38f795008
Dienay Lima <dienaylima@gmail.com>
2021-03-11 22:06:47 -0300

    retirada do contexto que seria utilizado para controlar rotas

--- code-challenge-preparo/src/App.js
+++ code-challenge-preparo/src/App.js
@@ -4,24 +4,12 @@ import { useHistory } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import NavContext from './Contexts/NavContext';
 
 function App() {
-  const history = useHistory()
-
-  const goToLogin = () => {
-      history.replace("/login")
-  }
-  
-  const goToSignup = () => {
-      history.replace("/signup")
-  }
   return (
-    <NavContext.Provider value={{goToLogin: goToLogin, goToSignup: goToSignup }}>
       <div className="app-container">
         <Router />
     </div>
-    </NavContext.Provider>
   )
 }
 
